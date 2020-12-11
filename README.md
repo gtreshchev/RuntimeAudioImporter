a8539b85a01114e0926a03a23cd98b7a0a75c017
Georgy Treshchev <gtreshchev@gmail.com>
2020-12-11 14:23:40 +0100

    add plugin data

---
+++ README.md
@@ -0,0 +1,31 @@
+![Runtime Audio Importer Unreal Engine Plugin Logo](image/runtimeaudioimporter.png "RuntimeAudioImporter Unreal Engine Plugin Logo")
+## Basic information
+This plugin will be useful for those who want to be able to import audio files of MP3, WAV and FLAC formats into the game in runtime (for example, if the game involves importing audio files from the player's device memory). It's easy to use in both C++ and Blueprints.
+
+![Runtime Audio Importer Unreal Engine Plugin Nodes](image/nodesexample.png "RuntimeAudioImporter Unreal Engine Plugin Nodes")
+
+## Features
+- Low library size (≈ 1 mb)
+- Fast transcoding speed (≈ 200-900 ms)
+- Supported for major audio formats: MP3, WAV and FLAC
+- Automatic detection of audio format (by extension)
+- System for obtaining transcoding status for error detection
+- No any third party libraries and external dependencies
+- Support for all available devices (tested on Windows and Android, but there are no restrictions to work on other devices)
+
+## How to install
+Just extract the "RuntimeAudioImporter" folder into your plugins project folder to get the following path: "[ProjectName]/Plugins/RuntimeAudioImporter".
+
+## How to use
+ There are two ways to use this plugin: using Blueprints and C++:
+ 1. Blueprints. Just reopen your Unreal project and find a node "GetSoundWaveFromAudioFile".
+ 2. C++. Open your "[ProjectName].Build.cs" and find a block of ` PublicDependencyModuleNames ` or ` PrivateDependencyModuleNames ` (it depends on your needs). Add `"RuntimeAudioImporter"` String inside this block. After just include "RuntimeAudioImporterBPLibrary.h" using ` #include "RuntimeAudioImporterBPLibrary.h" ` where needed.
+
+## Additional information
+The in-memory audio transcoding operation is based on [dr_libs](https://github.com/mackron/dr_libs).
+
+## Legal info
+
+Unreal® is a trademark or registered trademark of Epic Games, Inc. in the United States of America and elsewhere.
+
+Unreal® Engine, Copyright 1998 – 2020, Epic Games, Inc. All rights reserved.
---
+++ RuntimeAudioImporter/Source/RuntimeAudioImporter/Public/RuntimeAudioImporterBPLibrary.h
@@ -0,0 +1,69 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+
+#include "Runtime/Engine/Classes/Sound/SoundWave.h"
+//#include "Runtime/Engine/Public/VorbisAudioInfo.h"
+#include "Developer/TargetPlatform/Public/Interfaces/IAudioFormat.h"
+
+#include "Async/Async.h"
+
+#include "RuntimeAudioImporterBPLibrary.generated.h"
+
+
+
+
+UENUM(BlueprintType)
+enum TranscodingStatus
+{
+	Success						UMETA(DisplayName = "Success"),
+	FailedToOpenStream			UMETA(DisplayName = "Failed to open stream"),
+	UnsupportedBitDepth			UMETA(DisplayName = "Unsupported bit depth (Unreal Engine only supports 16 bit)"),
+	InvalidWAVEData				UMETA(DisplayName = "Invalid WAVE data"),
+	InvalidBitrate				UMETA(DisplayName = "Invalid bitrate (Actual bit rate does not match the container size)"),
+	InvalidSubformat			UMETA(DisplayName = "InvalidSubformat (Subformat identifier not recognized)"),
+	UnrecognizedReadWaveError   UMETA(DisplayName = "Unrecognized ReadWave error"),
+	USoundWaveDeclarationError	UMETA(DisplayName = "USoundWave declaration error"),
+	InvalidUSoundWave			UMETA(DisplayName = "Invalid USoundWave (The USoundWave object contains incorrect data)"),
+	InvalidAudioFormat			UMETA(DisplayName = "Invalid audio format (Can't determine the format of the audio file)"),
+	AudioDoesNotExist			UMETA(DisplayName = "The audio file does not exist"),
+	ZeroDurationError			UMETA(DisplayName = "Transcoded audio has zero length")
+};
+
+
+UENUM(BlueprintType)
+enum AudioFormat
+{
+	MP3							UMETA(DisplayName = "mp3"),
+	WAV							UMETA(DisplayName = "wav"),
+	FLAC						UMETA(DisplayName = "flac"),
+	INVALID						UMETA(DisplayName = "invalid (not defined format, internal use only)", Hidden)
+};
+
+
+UCLASS()
+class RUNTIMEAUDIOIMPORTER_API URuntimeAudioImporterBPLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_UCLASS_BODY()
+public:
+
+	/* Transcode audio file to USoundWave static object */
+	UFUNCTION(BlueprintCallable, meta = (Keywords = "Importer, Transcoder, Converter, MP3, FLAC, WAV", ToolTip = "Transcode audio file to USoundWave static object"), Category = "RuntimeAudioImporter")
+		static class USoundWave* GetSoundWaveFromAudioFile(const FString & filePath, TEnumAsByte < AudioFormat > Format, TEnumAsByte < TranscodingStatus > & status, bool DefineFormatAutomatically);
+
+private:
+
+	/* Main internal get USoundWave from audio file */
+	static class USoundWave* GetUSoundWaveFromAudioFile_Internal(const FString & filePath, TEnumAsByte < AudioFormat > Format, TEnumAsByte < TranscodingStatus > & status, bool DefineFormatAutomatically);
+
+	/* Get USoundWave static object reference */
+	static class USoundWave* GetSoundWaveObject(const uint8 * WaveData, int32 WaveDataSize, TEnumAsByte < TranscodingStatus > & status);
+
+	/* Transcode Audio to Wave Data */
+	static bool TranscodeAudioToWaveData(const char* filePath, TEnumAsByte < AudioFormat > Format, TEnumAsByte < TranscodingStatus > & status, uint64 & framesToWrite, int16_t * &pSampleData, uint32 & channels, uint32 & sampleRate);
+
+	/* Automatically get audio format. Internal use only recommended */
+	static TEnumAsByte<AudioFormat> GetAudioFormat(const FString & filePath);
+};
