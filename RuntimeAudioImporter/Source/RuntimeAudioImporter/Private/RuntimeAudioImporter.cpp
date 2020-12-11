830a78007a60d25f3c0f6f7039d010cff14bfb50
Georgy Treshchev <gtreshchev@gmail.com>
2020-12-11 14:23:40 +0100

    add plugin data

---
+++ RuntimeAudioImporter/Source/RuntimeAudioImporter/Private/RuntimeAudioImporter.cpp
@@ -0,0 +1,22 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RuntimeAudioImporter.h"
+
+#define LOCTEXT_NAMESPACE "FRuntimeAudioImporterModule"
+
+void FRuntimeAudioImporterModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+
+}
+
+void FRuntimeAudioImporterModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FRuntimeAudioImporterModule, RuntimeAudioImporter)
