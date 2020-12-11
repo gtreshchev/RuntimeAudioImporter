54559198f0b4771a3890d20178d12cd881c34386
Respirant <director@outech.ru>
2020-12-11 14:23:40 +0100

    add plugin data

---
+++ RuntimeAudioImporter/Source/RuntimeAudioImporter/Public/RuntimeAudioImporter.h
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Modules/ModuleManager.h"
+
+class FRuntimeAudioImporterModule : public IModuleInterface
+{
+public:
+
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+
+
+};
