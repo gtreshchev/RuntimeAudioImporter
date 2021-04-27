d7d0fe84bd384f8d13e1fcd9e8cb57dcc80823bc
Georgy Treshchev <gtreshchev@gmail.com>
2021-04-27 10:58:30 +0300

    Ability to get SamplingRate + dr_libs update and other minor design changes

--- RuntimeAudioImporter/Source/RuntimeAudioImporter/Public/ImportedSoundWave.h
+++ RuntimeAudioImporter/Source/RuntimeAudioImporter/Public/ImportedSoundWave.h
@@ -23,12 +23,18 @@ struct FPCMStruct
 
 	/** PCM data size */
 	uint32 PCMDataSize;
+
+	/** Base constructor */
+	FPCMStruct()
+		: PCMData(nullptr), PCMNumOfFrames(0), PCMDataSize(0)
+	{
+	}
 };
 
 /**
  * The main sound wave class used to play imported audio from the Runtime Audio Importer
  */
-UCLASS(BlueprintType, Category = "RuntimeAudioImporter")
+UCLASS(BlueprintType, Category = "Imported Sound Wave")
 class RUNTIMEAUDIOIMPORTER_API UImportedSoundWave : public USoundWaveProcedural
 {
 	GENERATED_BODY()
@@ -37,7 +43,7 @@ public:
 	/**
 	 * Bind to this delegate to know when the audio playback is finished
 	 */
-	UPROPERTY(BlueprintAssignable, Category = "RuntimeAudioImporter")
+	UPROPERTY(BlueprintAssignable, Category = "Imported Sound Wave")
 	FOnAudioPlaybackFinished OnAudioPlaybackFinished;
 
 	/**
@@ -54,7 +60,7 @@ public:
 	/**
 	 * Release PCM data. After the SoundWave is no longer needed, you need to call this function to free memory
 	 */
-	UFUNCTION(BlueprintCallable, Category = "RuntimeAudioImporter")
+	UFUNCTION(BlueprintCallable, Category = "Imported Sound Wave")
 	void ReleaseMemory();
 
 	/**
@@ -72,31 +78,37 @@ public:
 	 * @param NumOfFrames The new number of frames from which to continue playing sound
 	 * @return Whether the frames were changed or not
 	 */
-	UFUNCTION(Category = "RuntimeAudioImporter")
+	UFUNCTION(Category = "Imported Sound Wave")
 	bool ChangeCurrentFrameCount(const int32 NumOfFrames);
 
 	/**
 	 * Get the current sound playback time, in seconds
 	 */
-	UFUNCTION(BlueprintCallable, Category = "RuntimeAudioImporter")
+	UFUNCTION(BlueprintCallable, Category = "Imported Sound Wave")
 	float GetPlaybackTime() const;
 
 	/**
 	 * Get duration of a sound, in seconds
 	 */
-	UFUNCTION(BlueprintCallable, Category = "RuntimeAudioImporter")
+	UFUNCTION(BlueprintCallable, Category = "Imported Sound Wave")
 	float GetDuration() override;
 
 	/**
 	 * Get the current sound playback percentage, 0-100%
 	 */
-	UFUNCTION(BlueprintCallable, Category = "RuntimeAudioImporter")
+	UFUNCTION(BlueprintCallable, Category = "Imported Sound Wave")
 	float GetPlaybackPercentage();
+
+	/**
+	 * Sampling Rate (samples per second)
+	 */
+	UPROPERTY(BlueprintReadOnly, Category = "Imported Sound Wave")
+	int32 SamplingRate;
 
 	/**
 	 * Check if audio playback has finished or not
 	 */
-	UFUNCTION(BlueprintCallable, Category = "RuntimeAudioImporter")
+	UFUNCTION(BlueprintCallable, Category = "Imported Sound Wave")
 	bool IsPlaybackFinished();
 
 	/**
@@ -126,5 +138,6 @@ protected:
 	/**
 	 * The current number of frames
 	 */
-	UPROPERTY() int32 CurrentNumOfFrames = 0;
+	UPROPERTY()
+	int32 CurrentNumOfFrames = 0;
 };
