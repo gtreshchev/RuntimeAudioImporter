89c5b237fb8412f0e7dc8f3d78c6773e227d309c
Georgy Treshchev <gtreshchev@gmail.com>
2021-07-22 15:48:10 +0300

    Minor name changes

---
+++ RuntimeAudioImporter/Source/RuntimeAudioImporter/Public/PreImportedSoundAsset.h
@@ -0,0 +1,35 @@
+// Georgy Treshchev 2021.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PreImportedSoundAsset.generated.h"
+
+/**
+ * Pre-imported asset which collects MP3 audio data. Used if you want to load the MP3 file into the editor in advance
+ */
+UCLASS(BlueprintType, Category = "Runtime Audio Importer")
+class RUNTIMEAUDIOIMPORTER_API UPreImportedSoundAsset : public UObject
+{
+	GENERATED_BODY()
+public:
+
+	/** Audio data array. Only MP3 data is intended to be here */
+	UPROPERTY()
+	TArray<uint8> AudioDataArray;
+
+	/** Information about the basic details of an audio file. Used only for convenience in the editor */
+#if WITH_EDITORONLY_DATA
+	UPROPERTY(Category = "File Path", VisibleAnywhere, Meta = (DisplayName = "Source file path"))
+	FString SourceFilePath;
+
+	UPROPERTY(Category = "Info", VisibleAnywhere, Meta = (DisplayName = "Sound duration"))
+	FString SoundDuration;
+
+	UPROPERTY(Category = "Info", VisibleAnywhere, Meta = (DisplayName = "Number of channels"))
+	int32 NumberOfChannels;
+
+	UPROPERTY(Category = "Info", VisibleAnywhere, Meta = (DisplayName = "Sample rate"))
+	int32 SampleRate;
+#endif
+};
---
+++ RuntimeAudioImporter/Source/RuntimeAudioImporterEditor/Private/PreImportedSoundFactory.cpp
@@ -0,0 +1,125 @@
+// Georgy Treshchev 2021.
+
+#include "PreimportedSoundFactory.h"
+
+#include "PreImportedSoundAsset.h"
+
+#include "Misc/FileHelper.h"
+
+/** Defining custom logging */
+DEFINE_LOG_CATEGORY(LogPreimportedSoundFactory)
+
+#include "ThirdParty/dr_mp3.h"
+
+/**
+* Replacing standard CPP memory functions (malloc, realloc, free) with memory management functions that are maintained by Epic as the engine evolves.
+*/
+void* Unreal_Malloc(size_t sz, void* pUserData)
+{
+	return FMemory::Malloc(sz);
+}
+
+void* Unreal_Realloc(void* p, size_t sz, void* pUserData)
+{
+	return FMemory::Realloc(p, sz);
+}
+
+void Unreal_Free(void* p, void* pUserData)
+{
+	FMemory::Free(p);
+}
+
+UPreImportedSoundFactory::UPreImportedSoundFactory()
+{
+	Formats.Add(TEXT("mp3;MP3 Preimported Audio Format"));
+	Formats.Add(TEXT("imp;IMP (the same as MP3) Preimported Audio Format"));
+	SupportedClass = StaticClass();
+	bCreateNew = false; // turned off for import
+	bEditAfterNew = false; // turned off for import
+	bEditorImport = true;
+	bText = false;
+}
+
+bool UPreImportedSoundFactory::FactoryCanImport(const FString& Filename)
+{
+	return FPaths::GetExtension(Filename).Equals(TEXT("mp3")) || FPaths::GetExtension(Filename).Equals(TEXT("imp"));
+}
+
+UObject* UPreImportedSoundFactory::FactoryCreateFile(UClass* InClass, UObject* InParent, FName InName,
+                                                     EObjectFlags Flags, const FString& Filename, const TCHAR* Parms,
+                                                     FFeedbackContext* Warn, bool& bOutOperationCanceled)
+{
+	UPreImportedSoundAsset* PreImportedSoundAsset;
+
+	TArray<uint8> AudioDataArray;
+
+	if (FFileHelper::LoadFileToArray(AudioDataArray, *Filename))
+	{
+		drmp3_allocation_callbacks allocationCallbacksDecoding;
+		allocationCallbacksDecoding.pUserData = nullptr;
+		allocationCallbacksDecoding.onMalloc = Unreal_Malloc;
+		allocationCallbacksDecoding.onRealloc = Unreal_Realloc;
+		allocationCallbacksDecoding.onFree = Unreal_Free;
+
+		drmp3 mp3;
+
+		if (!drmp3_init_memory(&mp3, AudioDataArray.GetData(), AudioDataArray.Num() - 2, &allocationCallbacksDecoding))
+		{
+			UE_LOG(LogPreimportedSoundFactory, Log, TEXT("The file you are trying to import is not an MP3 file."));
+			return nullptr;
+		}
+
+		PreImportedSoundAsset = NewObject<UPreImportedSoundAsset>(InParent, InClass, InName, Flags);
+		PreImportedSoundAsset->AudioDataArray = AudioDataArray;
+		PreImportedSoundAsset->SourceFilePath = Filename;
+
+		PreImportedSoundAsset->SoundDuration = ConvertSecToFormattedDuration(
+			static_cast<int32>(drmp3_get_pcm_frame_count(&mp3)) / mp3.sampleRate);
+		UE_LOG(LogPreimportedSoundFactory, Log, TEXT("Duration: %f"),
+		       (static_cast<float>(drmp3_get_pcm_frame_count(&mp3)) / mp3.sampleRate));
+		UE_LOG(LogTemp, Warning, TEXT("Duration: %f"),
+		       (static_cast<float>(drmp3_get_pcm_frame_count(&mp3)) / mp3.sampleRate));
+		PreImportedSoundAsset->NumberOfChannels = mp3.channels;
+		PreImportedSoundAsset->SampleRate = mp3.sampleRate;
+
+		drmp3_uninit(&mp3);
+	}
+	else
+	{
+		UE_LOG(LogPreimportedSoundFactory, Log, TEXT("Unable to read the audio file. Check file permissions."));
+		return nullptr;
+	}
+
+	bOutOperationCanceled = false;
+
+	return PreImportedSoundAsset;
+}
+
+FString UPreImportedSoundFactory::ConvertSecToFormattedDuration(int32 Seconds)
+{
+	FString FinalString;
+
+	const int32 NewHours = Seconds / 3600;
+	if (NewHours > 0)
+	{
+		FinalString += ((NewHours < 10) ? TEXT("0") + FString::FromInt(NewHours) : FString::FromInt(NewHours)) +
+			TEXT(":");
+	}
+
+	Seconds = Seconds % 3600;
+
+	const int32 NewMinutes = Seconds / 60;
+	if (NewMinutes > 0)
+	{
+		FinalString += ((NewMinutes < 10) ? TEXT("0") + FString::FromInt(NewMinutes) : FString::FromInt(NewMinutes)) +
+			TEXT(":");
+	}
+
+	const int32 NewSeconds = Seconds % 60;
+	if (NewSeconds > 0)
+	{
+		FinalString += (NewSeconds < 10) ? TEXT("0") + FString::FromInt(NewSeconds) : FString::FromInt(NewSeconds);
+	}
+
+	return FinalString;
+}
