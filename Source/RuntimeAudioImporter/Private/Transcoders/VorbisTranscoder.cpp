7887d567f90e98135149e92482fad7e87a938f91
Georgy Treshchev <gtreshchev@gmail.com>
2022-01-24 08:57:33 +0300

    Fix OGG Vorbis decoding error, adding the functionality to bind to a delegate to get PCM data during playback

--- Source/RuntimeAudioImporter/Private/Transcoders/VorbisTranscoder.cpp
+++ Source/RuntimeAudioImporter/Private/Transcoders/VorbisTranscoder.cpp
@@ -2,6 +2,7 @@
 
 #include "Transcoders/VorbisTranscoder.h"
 #include "RuntimeAudioImporterTypes.h"
+#include "Transcoders/RAWTranscoder.h"
 
 #define INCLUDE_VORBIS
 #include "RuntimeAudioImporterIncludes.h"
@@ -9,61 +10,38 @@
 
 bool VorbisTranscoder::Decode(FEncodedAudioStruct EncodedData, FDecodedAudioStruct& DecodedData)
 {
-	stb_vorbis* STB_Vorbis{stb_vorbis_open_memory(EncodedData.AudioData, EncodedData.AudioDataSize, nullptr, nullptr)};
+	short* DecodedBufferInt16Buffer;
+	int32 SampleRate, NumOfChannels;
 
-	if (STB_Vorbis == nullptr)
+	/** Decoding the data */
+	const int32 NumOfFrames = stb_vorbis_decode_memory(EncodedData.AudioData, EncodedData.AudioDataSize, &NumOfChannels, &SampleRate, &DecodedBufferInt16Buffer);
+
+	/** Checking if we got a valid data */
+	if (DecodedBufferInt16Buffer == nullptr || NumOfFrames == -1)
 	{
 		return false;
 	}
 
-	const stb_vorbis_info& info{stb_vorbis_get_info(STB_Vorbis)};
-
-	const int32 SampleCount = stb_vorbis_stream_length_in_samples(STB_Vorbis);
-
-	DecodedData.PCMInfo.PCMData = reinterpret_cast<uint8*>(new float[SampleCount * info.channels]);
-
-	DecodedData.PCMInfo.PCMNumOfFrames = 0;
-
-	TArray<float> PCMData_Array;
-
-	while (true)
-	{
-		float** DecodedBufferInFrame;
-		const int32 NumOfSamplesInFrame = stb_vorbis_get_frame_float(STB_Vorbis, nullptr, &DecodedBufferInFrame);
-
-		if (NumOfSamplesInFrame == 0)
-		{
-			break;
-		}
-
-		for (int32 CurrentSampleInFrame = 0; CurrentSampleInFrame < NumOfSamplesInFrame; ++CurrentSampleInFrame)
-		{
-			for (int32 Channel = 0; Channel < info.channels; ++Channel)
-			{
-				PCMData_Array.Emplace(DecodedBufferInFrame[Channel][CurrentSampleInFrame]);
-			}
-		}
-
-		/** Filling the number of frames */
-		DecodedData.PCMInfo.PCMNumOfFrames += NumOfSamplesInFrame;
-	}
-
-	FMemory::Memcpy(DecodedData.PCMInfo.PCMData, PCMData_Array.GetData(), PCMData_Array.Num() - 2);
-
-	/** Clearing unused buffer */
-	PCMData_Array.Empty();
-
-	/** Uninitializing transcoding of audio data in memory */
-	stb_vorbis_close(STB_Vorbis);
+	/** Getting the number of frames */
+	DecodedData.PCMInfo.PCMNumOfFrames = NumOfFrames;
 
 	/** Getting PCM data size */
-	DecodedData.PCMInfo.PCMDataSize = static_cast<uint32>(DecodedData.PCMInfo.PCMNumOfFrames * info.channels * sizeof(float));
+	DecodedData.PCMInfo.PCMDataSize = static_cast<uint32>(DecodedData.PCMInfo.PCMNumOfFrames * NumOfChannels);
+
+	/** Transcoding int16 to float format */
+	{
+		float* TempFloatBuffer = new float[DecodedData.PCMInfo.PCMNumOfFrames * NumOfChannels];
+		uint32 TempFloatSize;
+		
+		RAWTranscoder::TranscodeRAWData<int16, float>(DecodedBufferInt16Buffer, DecodedData.PCMInfo.PCMDataSize, TempFloatBuffer, TempFloatSize);
+		DecodedData.PCMInfo.PCMData = reinterpret_cast<uint8*>(TempFloatBuffer);
+	}
 
 	/** Getting basic audio information */
 	{
-		DecodedData.SoundWaveBasicInfo.Duration = static_cast<float>(DecodedData.PCMInfo.PCMNumOfFrames) / info.sample_rate;
-		DecodedData.SoundWaveBasicInfo.ChannelsNum = info.channels;
-		DecodedData.SoundWaveBasicInfo.SampleRate = info.sample_rate;
+		DecodedData.SoundWaveBasicInfo.Duration = static_cast<float>(DecodedData.PCMInfo.PCMNumOfFrames) / SampleRate;
+		DecodedData.SoundWaveBasicInfo.ChannelsNum = NumOfChannels;
+		DecodedData.SoundWaveBasicInfo.SampleRate = SampleRate;
 	}
 
 	return true;
