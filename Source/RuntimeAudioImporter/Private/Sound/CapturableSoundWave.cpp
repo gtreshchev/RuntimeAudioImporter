gtreshchev/RuntimeAudioImporter

commit bc628baf25206b294f5bfa8e8d779eeda5bc85d3

Support for iOS in CapturableSoundWave and minor adjustments to the BINK codec

Source/RuntimeAudioImporter/Private/Sound/CapturableSoundWave.cpp
@@ -1,6 +1,16 @@
 ﻿// Georgy Treshchev 2023.
 
 #include "Sound/CapturableSoundWave.h"
+
+#if WITH_RUNTIMEAUDIOIMPORTER_CAPTURE_SUPPORT
+
+// Untested thus disabled for now
+#if false && PLATFORM_ANDROID
+#include "AndroidPermissionCallbackProxy.h"
+#include "AndroidPermissionFunctionLibrary.h"
+#endif
+
+#endif
 #include "RuntimeAudioImporterDefines.h"
 #include "AudioThread.h"
 #include "Async/Async.h"
@@ -13,8 +23,13 @@ UCapturableSoundWave::UCapturableSoundWave(const FObjectInitializer& ObjectIniti
 void UCapturableSoundWave::BeginDestroy()
 {
 #if WITH_RUNTIMEAUDIOIMPORTER_CAPTURE_SUPPORT
+#if PLATFORM_IOS && !PLATFORM_TVOS
+	AudioCaptureIOS.AbortStream();
+	AudioCaptureIOS.CloseStream();
+#else
 	AudioCapture.AbortStream();
 	AudioCapture.CloseStream();
+#endif
 #endif
 
 	Super::BeginDestroy();
@@ -82,11 +97,33 @@ void UCapturableSoundWave::GetAvailableAudioInputDevices(const FOnGetAvailableAu
 bool UCapturableSoundWave::StartCapture(int32 DeviceId)
 {
 #if WITH_RUNTIMEAUDIOIMPORTER_CAPTURE_SUPPORT
-	if (AudioCapture.IsStreamOpen())
+// Untested thus disabled for now
+#if false && PLATFORM_ANDROID
+	TArray<FString> Permissions = {"android.permission.RECORD_AUDIO"};
+	if (!UAndroidPermissionFunctionLibrary::CheckPermission(Permissions[0]))
 	{
-		UE_LOG(LogRuntimeAudioImporter, Error, TEXT("Unable to start capture as the stream is already open"));
-		return false;
+		TPromise<bool> bPermissionGrantedPromise;
+		TFuture<bool> bPermissionGrantedFuture = bPermissionGrantedPromise.GetFuture();
+		UAndroidPermissionFunctionLibrary::AcquirePermissions(Permissions)->OnPermissionsGrantedDelegate.AddWeakLambda(this, [this, bPermissionGrantedPromise = MoveTemp(bPermissionGrantedPromise), Permissions = MoveTemp(Permissions)](const TArray<FString>& GrantPermissions, const TArray<bool>& GrantResults) mutable
+		{
+			if (GrantPermissions.Find(Permissions[0]) == INDEX_NONE || GrantResults.Find(true) != INDEX_NONE)
+			{
+				UE_LOG(LogRuntimeAudioImporter, Error, TEXT("Unable to start capture as the permission was not granted"));
+				bPermissionGrantedPromise.SetValue(false);
+				return;
+			}
+			bPermissionGrantedPromise.SetValue(true);
+		});
+		bPermissionGrantedFuture.Wait();
+		if (!bPermissionGrantedFuture.Get())
+		{
+			return false;
+		}
 	}
+#endif
+	
+	Audio::FAudioCaptureDeviceParams Params = Audio::FAudioCaptureDeviceParams();
+	Params.DeviceIndex = DeviceId;
 
 	Audio::FOnCaptureFunction OnCapture = [this](const float* PCMData, int32 NumFrames, int32 NumOfChannels,
 #if UE_VERSION_NEWER_THAN(4, 25, 0)
@@ -94,29 +131,47 @@ bool UCapturableSoundWave::StartCapture(int32 DeviceId)
 #endif
 	                                             double StreamTime, bool bOverFlow)
 	{
+#if PLATFORM_IOS && !PLATFORM_TVOS
+		if (AudioCaptureIOS.IsCapturing())
+#else
 		if (AudioCapture.IsCapturing())
+#endif
 		{
 			const int64 PCMDataSize = NumOfChannels * NumFrames;
-			const int64 PCMDataSizeInBytes = PCMDataSize * sizeof(float);
+			int64 PCMDataSizeInBytes = PCMDataSize * sizeof(float);
 
 			if (PCMDataSizeInBytes > TNumericLimits<int32>::Max())
 			{
-				UE_LOG(LogRuntimeAudioImporter, Error, TEXT("Unable to append audio data as the size of the data exceeds the maximum size of int32 (PCMDataSizeInBytes: %lld, Max: %d)"), PCMDataSizeInBytes, TNumericLimits<int32>::Max());
-				return;
+				UE_LOG(LogRuntimeAudioImporter, Warning, TEXT("Unable to append audio data as the size of the data exceeds the maximum size of int32 (PCMDataSizeInBytes: %lld, Max: %d)"), PCMDataSizeInBytes, TNumericLimits<int32>::Max());
+				PCMDataSizeInBytes = TNumericLimits<int32>::Max();
 			}
 
 			AppendAudioDataFromRAW(TArray<uint8>(reinterpret_cast<const uint8*>(PCMData), static_cast<int32>(PCMDataSizeInBytes)), ERuntimeRAWAudioFormat::Float32,
 #if UE_VERSION_NEWER_THAN(4, 25, 0)
 									InSampleRate
 #else
+#if PLATFORM_IOS && !PLATFORM_TVOS
+			                       AudioCaptureIOS.GetSampleRate()
+#else
 			                       AudioCapture.GetSampleRate()
+#endif
 #endif
 			                     , NumOfChannels);
 		}
 	};
 
-	Audio::FAudioCaptureDeviceParams Params = Audio::FAudioCaptureDeviceParams();
-	Params.DeviceIndex = DeviceId;
+#if PLATFORM_IOS && !PLATFORM_TVOS
+#if (defined(__IPHONE_17_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_17_0)
+	return [[AVAudioApplication sharedInstance] recordPermission] == AVAudioApplicationRecordPermissionGranted;
+#else
+	return [[AVAudioSession sharedInstance] recordPermission] == AVAudioSessionRecordPermissionGranted;
+#endif
+#else
+	if (AudioCapture.IsStreamOpen())
+	{
+		UE_LOG(LogRuntimeAudioImporter, Error, TEXT("Unable to start capture as the stream is already open"));
+		return false;
+	}
 
 	if (!AudioCapture.OpenCaptureStream(Params, MoveTemp(OnCapture), 1024))
 	{
@@ -132,6 +187,7 @@ bool UCapturableSoundWave::StartCapture(int32 DeviceId)
 
 	UE_LOG(LogRuntimeAudioImporter, Log, TEXT("Successfully started capturing for sound wave %s"), *GetName());
 	return true;
+#endif
 #else
 	UE_LOG(LogRuntimeAudioImporter, Error, TEXT("Unable to start capturing as its support is disabled (please enable in RuntimeAudioImporter.Build.cs)"));
 	return false;
@@ -141,10 +197,17 @@ bool UCapturableSoundWave::StartCapture(int32 DeviceId)
 void UCapturableSoundWave::StopCapture()
 {
 #if WITH_RUNTIMEAUDIOIMPORTER_CAPTURE_SUPPORT
+#if PLATFORM_IOS && !PLATFORM_TVOS
+	if (AudioCaptureIOS.IsStreamOpen())
+	{
+		AudioCaptureIOS.CloseStream();
+	}
+#else
 	if (AudioCapture.IsStreamOpen())
 	{
 		AudioCapture.CloseStream();
 	}
+#endif
 #else
 	UE_LOG(LogRuntimeAudioImporter, Error, TEXT("Unable to stop capturing as its support is disabled (please enable in RuntimeAudioImporter.Build.cs)"));
 #endif
@@ -153,19 +216,31 @@ void UCapturableSoundWave::StopCapture()
 bool UCapturableSoundWave::ToggleMute(bool bMute)
 {
 #if WITH_RUNTIMEAUDIOIMPORTER_CAPTURE_SUPPORT
+#if PLATFORM_IOS && !PLATFORM_TVOS
+	if (!AudioCaptureIOS.IsStreamOpen())
+#else
 	if (!AudioCapture.IsStreamOpen())
+#endif
 	{
 		UE_LOG(LogRuntimeAudioImporter, Error, TEXT("Unable to toggle mute for %s as the stream is not open"), *GetName());
 		return false;
 	}
 	if (bMute)
 	{
+#if PLATFORM_IOS && !PLATFORM_TVOS
+		if (!AudioCaptureIOS.IsCapturing())
+#else
 		if (!AudioCapture.IsCapturing())
+#endif
 		{
 			UE_LOG(LogRuntimeAudioImporter, Error, TEXT("Unable to mute as the stream for %s is already closed"), *GetName());
 			return false;
 		}
+#if PLATFORM_IOS && !PLATFORM_TVOS
+		if (!AudioCaptureIOS.StopStream())
+#else
 		if (!AudioCapture.StopStream())
+#endif
 		{
 			UE_LOG(LogRuntimeAudioImporter, Error, TEXT("Unable to mute the stream for sound wave %s"), *GetName());
 			return false;
@@ -175,12 +250,20 @@ bool UCapturableSoundWave::ToggleMute(bool bMute)
 	}
 	else
 	{
-		if (AudioCapture.IsCapturing())
+#if PLATFORM_IOS && !PLATFORM_TVOS
+		if (!AudioCaptureIOS.IsCapturing())
+#else
+		if (!AudioCapture.IsCapturing())
+#endif
 		{
 			UE_LOG(LogRuntimeAudioImporter, Error, TEXT("Unable to unmute as the stream for %s is already open"), *GetName());
 			return false;
 		}
+#if PLATFORM_IOS && !PLATFORM_TVOS
+		if (!AudioCaptureIOS.StartStream())
+#else
 		if (!AudioCapture.StartStream())
+#endif
 		{
 			UE_LOG(LogRuntimeAudioImporter, Error, TEXT("Unable to unmute the stream for sound wave %s"), *GetName());
 			return false;
@@ -197,7 +280,11 @@ bool UCapturableSoundWave::ToggleMute(bool bMute)
 bool UCapturableSoundWave::IsCapturing() const
 {
 #if WITH_RUNTIMEAUDIOIMPORTER_CAPTURE_SUPPORT
+#if PLATFORM_IOS && !PLATFORM_TVOS
+	return AudioCaptureIOS.IsCapturing();
+#else
 	return AudioCapture.IsCapturing();
+#endif
 #else
 	UE_LOG(LogRuntimeAudioImporter, Error, TEXT("Unable to get capturing state as its support is disabled (please enable in RuntimeAudioImporter.Build.cs)"));
 	return false;

Source/RuntimeAudioImporter/Private/Codecs/BINK_RuntimeCodec.cpp
@@ -15,7 +15,7 @@
 #include "CodecIncludes.h"
 #undef INCLUDE_BINK
 
-
+#if WITH_RUNTIMEAUDIOIMPORTER_BINK_ENCODE_SUPPORT
 /**
  * Taken from AudioFormatBink.cpp
  */
@@ -27,11 +27,15 @@ namespace
 		static constexpr float BinkLowest = 4;
 		static constexpr float BinkHighest = 0;
 
-		// Map Quality 1 (lowest) to 40 (highest)
+		// Map Quality 1 (lowest) to 100 (highest).
 		static constexpr float QualityLowest = 1;
-		static constexpr float QualityHighest = 40;
+		static constexpr float QualityHighest = 100;
 
-		return FMath::GetMappedRangeValueClamped(FVector2D(QualityLowest, QualityHighest), FVector2D(BinkLowest, BinkHighest), InQualityIndex);
+		// Map Quality into Bink Range. Note: +1 gives the Bink range 5 steps inclusive.
+		const float BinkValue = FMath::GetMappedRangeValueClamped(FVector2D(QualityLowest, QualityHighest), FVector2D(BinkLowest + 1.f, BinkHighest), InQualityIndex);
+
+		// Floor each value and clamp into range (as top lerp will be +1 over)
+		return FMath::Clamp(FMath::FloorToInt(BinkValue), BinkHighest, BinkLowest);
 	}
 
 	void* BinkAlloc(size_t Bytes)
@@ -75,6 +79,7 @@ namespace
 	}
 #endif
 }
+#endif
 
 bool FBINK_RuntimeCodec::CheckAudioFormat(const FRuntimeBulkDataBuffer<uint8>& AudioData)
 {
@@ -145,6 +150,7 @@ bool FBINK_RuntimeCodec::Encode(FDecodedAudioStruct DecodedData, FEncodedAudioSt
 
 	void* CompressedData = nullptr;
 	uint32_t CompressedDataLen = 0;
+
 	UECompressBinkAudio(static_cast<void*>(TempInt16Buffer), NumOfSamplesInBytes, DecodedData.SoundWaveBasicInfo.SampleRate, DecodedData.SoundWaveBasicInfo.NumOfChannels, CompressionLevel, 1,
 #if UE_VERSION_NEWER_THAN(5, 3, 0)
 		MaxSeektableSize,

Source/RuntimeAudioImporter/Private/Sound/IOS/AudioCaptureIOS.cpp
@@ -1,0 +1,330 @@
+// Georgy Treshchev 2023.
+
+#if PLATFORM_IOS && !PLATFORM_TVOS
+#include "Sound/IOS/AudioCaptureIOS.h"
+
+#include "RuntimeAudioImporterDefines.h"
+
+constexpr int32 kInputBus = 1;
+constexpr int32 kOutputBus = 0;
+
+static OSStatus RecordingCallback(void *inRefCon,
+                                  AudioUnitRenderActionFlags *ioActionFlags,
+                                  const AudioTimeStamp *inTimeStamp,
+                                  UInt32 inBusNumber,
+                                  UInt32 inNumberFrames,
+                                  AudioBufferList *ioData)
+{
+	Audio::FAudioCaptureIOS* AudioCapture = (Audio::FAudioCaptureIOS*)inRefCon;
+	return AudioCapture->OnCaptureCallback(ioActionFlags, inTimeStamp, inBusNumber, inNumberFrames, ioData);
+}
+
+bool Audio::FAudioCaptureIOS::GetCaptureDeviceInfo(Audio::FCaptureDeviceInfo& OutInfo, int32 DeviceIndex)
+{
+	OutInfo.DeviceName = TEXT("Default iOS Audio Device");
+	OutInfo.InputChannels = NumChannels;
+	OutInfo.PreferredSampleRate = SampleRate;
+
+	return true;
+}
+
+
+bool Audio::FAudioCaptureIOS::OpenCaptureStream(const Audio::FAudioCaptureDeviceParams& InParams, Audio::FOnCaptureFunction InOnCapture, uint32 NumFramesDesired)
+{
+	if (bIsStreamOpen)
+	{
+		UE_LOG(LogRuntimeAudioImporter, Error, TEXT("Unable to open capture stream because it is already open"));
+		return true;
+	}
+
+	OnCapture = MoveTemp(InOnCapture);
+
+	OSStatus Status = noErr;
+
+	NSError *setCategoryError = nil;
+	[[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord withOptions : (AVAudioSessionCategoryOptionDefaultToSpeaker | AVAudioSessionCategoryOptionAllowBluetooth) error : &setCategoryError];
+	if (setCategoryError != nil)
+	{		
+		UE_LOG(LogRuntimeAudioImporter, Error, TEXT("Unable to open capture stream due to error %d when setting audio session category"), static_cast<int32>(Status));
+		return false;
+	}
+
+	[[AVAudioSession sharedInstance] setActive:YES error : &setCategoryError];
+	if (setCategoryError != nil)
+	{
+		UE_LOG(LogRuntimeAudioImporter, Error, TEXT("Unable to open capture stream due to error %d when setting audio session active"), static_cast<int32>(Status));
+		return false;
+	}
+
+	// Source of info "Technical Note TN2091 - Device input using the HAL Output Audio Unit"
+
+	AudioComponentDescription desc;
+	desc.componentType = kAudioUnitType_Output;
+	// We use processing element always for enable runtime changing HW AEC and AGC settings.
+	// When it's disable the unit work as RemoteIO
+	desc.componentSubType = kAudioUnitSubType_VoiceProcessingIO;
+	desc.componentManufacturer = kAudioUnitManufacturer_Apple;
+	desc.componentFlags = 0;
+	desc.componentFlagsMask = 0;
+
+	AudioComponent InputComponent = AudioComponentFindNext(NULL, &desc);
+
+	Status = AudioComponentInstanceNew(InputComponent, &IOUnit);
+	if (Status != noErr)
+	{
+		UE_LOG(LogRuntimeAudioImporter, Error, TEXT("Unable to open capture stream due to error %d when creating audio component instance"), static_cast<int32>(Status));
+		return false;
+	}
+
+	// Enable recording
+	uint32 EnableIO = 1;
+	Status = AudioUnitSetProperty(IOUnit,
+		kAudioOutputUnitProperty_EnableIO,
+		kAudioUnitScope_Input,
+		kInputBus,
+		&EnableIO,
+		sizeof(EnableIO));
+
+	if (Status != noErr)
+	{
+		UE_LOG(LogRuntimeAudioImporter, Error, TEXT("Unable to open capture stream due to error %d when enabling input"), static_cast<int32>(Status));
+		return false;
+	}
+
+	// Disable output part
+	EnableIO = 0;
+	Status = AudioUnitSetProperty(IOUnit,
+		kAudioOutputUnitProperty_EnableIO,
+		kAudioUnitScope_Output,
+		kOutputBus,
+		&EnableIO,
+		sizeof(EnableIO));
+
+	if (Status != noErr)
+	{
+		UE_LOG(LogRuntimeAudioImporter, Error, TEXT("Unable to open capture stream due to error %d when disabling output"), static_cast<int32>(Status));
+		return false;
+	}
+
+	AudioStreamBasicDescription StreamDescription = { 0 };
+	const UInt32 BytesPerSample = sizeof(Float32);
+
+	StreamDescription.mSampleRate = SampleRate;
+	StreamDescription.mFormatID = kAudioFormatLinearPCM;
+	StreamDescription.mFormatFlags = kAudioFormatFlagsNativeFloatPacked;
+	StreamDescription.mChannelsPerFrame = NumChannels;
+	StreamDescription.mBitsPerChannel = 8 * BytesPerSample;
+	StreamDescription.mBytesPerFrame = BytesPerSample * StreamDescription.mChannelsPerFrame;
+	StreamDescription.mFramesPerPacket = 1;
+	StreamDescription.mBytesPerPacket = StreamDescription.mFramesPerPacket * StreamDescription.mBytesPerFrame;
+
+	// Configure output format
+	Status = AudioUnitSetProperty(IOUnit,
+		kAudioUnitProperty_StreamFormat,
+		kAudioUnitScope_Output,
+		kInputBus,
+		&StreamDescription,
+		sizeof(StreamDescription));
+
+	if (Status != noErr)
+	{
+		UE_LOG(LogRuntimeAudioImporter, Error, TEXT("Unable to open capture stream due to error %d when setting output format"), static_cast<int32>(Status));
+		return false;
+	}
+
+	// Setup capture callback
+	AURenderCallbackStruct CallbackInfo;
+	CallbackInfo.inputProc = RecordingCallback;
+	CallbackInfo.inputProcRefCon = this;
+	Status = AudioUnitSetProperty(IOUnit,
+		kAudioOutputUnitProperty_SetInputCallback,
+		kAudioUnitScope_Global,
+		kInputBus,
+		&CallbackInfo,
+		sizeof(CallbackInfo));
+	if (Status != noErr) {		
+		UE_LOG(LogRuntimeAudioImporter, Error, TEXT("Unable to open capture stream due to error %d when setting callback"), static_cast<int32>(Status));
+		return false;
+	}
+
+	// Initialize audio unit
+	Status = AudioUnitInitialize(IOUnit);
+	if (Status != noErr)
+	{
+		UE_LOG(LogRuntimeAudioImporter, Error, TEXT("Unable to open capture stream due to error %d when initializing audio unit"), static_cast<int32>(Status));
+		return false;
+	}
+
+	// Configure unit processing
+#if UE_VERSION_NEWER_THAN(4, 25, 0)
+    SetHardwareFeatureEnabled(Audio::EHardwareInputFeature::EchoCancellation, InParams.bUseHardwareAEC);
+    SetHardwareFeatureEnabled(Audio::EHardwareInputFeature::AutomaticGainControl, InParams.bUseHardwareAEC);
+#endif
+
+	bIsStreamOpen = (Status == noErr);
+	return bIsStreamOpen;
+}
+
+bool Audio::FAudioCaptureIOS::CloseStream()
+{
+	StopStream();
+	AudioComponentInstanceDispose(IOUnit);
+	return true;
+}
+
+bool Audio::FAudioCaptureIOS::StartStream()
+{
+	if (!IsStreamOpen() || IsCapturing())
+	{
+		return false;
+	}
+	AudioUnitReset(IOUnit, kAudioUnitScope_Global, 0);
+	bHasCaptureStarted = (AudioOutputUnitStart(IOUnit) == noErr);
+	return bHasCaptureStarted;
+}
+
+bool Audio::FAudioCaptureIOS::StopStream()
+{
+	if (!IsStreamOpen() || !IsCapturing())
+	{
+		return false;
+	}
+	const bool bStopped = (AudioOutputUnitStop(IOUnit) == noErr);
+	bHasCaptureStarted = !bStopped;
+	return bStopped;
+}
+
+bool Audio::FAudioCaptureIOS::AbortStream()
+{
+	StopStream();
+	CloseStream();
+	return true;
+}
+
+bool Audio::FAudioCaptureIOS::GetStreamTime(double& OutStreamTime)
+{
+	OutStreamTime = 0.0f;
+	return true;
+}
+
+bool Audio::FAudioCaptureIOS::IsStreamOpen() const
+{
+	return bIsStreamOpen;
+}
+
+bool Audio::FAudioCaptureIOS::IsCapturing() const
+{
+	return bHasCaptureStarted;
+}
+
+void Audio::FAudioCaptureIOS::OnAudioCapture(void* InBuffer, uint32 InBufferFrames, double StreamTime, bool bOverflow)
+{
+	OnCapture(static_cast<float*>(InBuffer), InBufferFrames, NumChannels, SampleRate, StreamTime, bOverflow);
+}
+
+bool Audio::FAudioCaptureIOS::GetInputDevicesAvailable(TArray<FCaptureDeviceInfo>& OutDevices)
+{
+	OutDevices.Reset();
+
+	FCaptureDeviceInfo& DeviceInfo = OutDevices.AddDefaulted_GetRef();
+	GetCaptureDeviceInfo(DeviceInfo, 0);
+
+	return true;
+}
+
+#if UE_VERSION_NEWER_THAN(4, 25, 0)
+void Audio::FAudioCaptureIOS::SetHardwareFeatureEnabled(Audio::EHardwareInputFeature FeatureType, bool bEnabled)
+{
+	if (IOUnit == nil)
+	{
+		return;
+	}
+
+	// we ignore result those functions because sometime we can't set this parameters
+	OSStatus status = noErr;
+
+	switch (FeatureType)
+	{
+	case Audio::EHardwareInputFeature::EchoCancellation:
+	{
+		const UInt32 EnableParam = (bEnabled ? 0 : 1);
+		status = AudioUnitSetProperty(IOUnit,
+			kAUVoiceIOProperty_BypassVoiceProcessing,
+			kAudioUnitScope_Global,
+			kInputBus,
+			&EnableParam,
+			sizeof(EnableParam)
+		);
+	}
+	break;
+	case Audio::EHardwareInputFeature::AutomaticGainControl:
+	{
+		const UInt32 EnableParam = (bEnabled ? 1 : 0);
+		status = AudioUnitSetProperty(IOUnit,
+			kAUVoiceIOProperty_VoiceProcessingEnableAGC,
+			kAudioUnitScope_Global,
+			kInputBus,
+			&EnableParam,
+			sizeof(EnableParam)
+		);
+	}
+	break;
+	}
+}
+#endif
+
+OSStatus Audio::FAudioCaptureIOS::OnCaptureCallback(AudioUnitRenderActionFlags* ioActionFlags, const AudioTimeStamp* inTimeStamp, UInt32 inBusNumber, UInt32 inNumberFrames, AudioBufferList* ioData)
+{
+	OSStatus status = noErr;
+
+	const int32 NeededBufferSize = inNumberFrames * NumChannels * sizeof(float);
+	if (CaptureBuffer.Num() == 0 || BufferSize < NeededBufferSize)
+	{
+		BufferSize = NeededBufferSize;
+		AllocateBuffer(BufferSize);
+	}
+
+	AudioBufferList* BufferList = (AudioBufferList*)CaptureBuffer.GetData();
+	for (int i = 0; i < BufferList->mNumberBuffers; ++i)
+	{
+		BufferList->mBuffers[i].mDataByteSize = (UInt32)BufferSize;
+	}
+
+	status = AudioUnitRender(IOUnit,
+		ioActionFlags,
+		inTimeStamp,
+		inBusNumber,
+		inNumberFrames,
+		BufferList);
+
+	if (status != noErr)
+	{
+		UE_LOG(LogRuntimeAudioImporter, Error, TEXT("Unable to process audio unit render callback for capture device: %d"), static_cast<int32>(status));
+		return 0;
+	}
+
+	void* InBuffer = (void*)BufferList->mBuffers[0].mData; // only first channel ?!
+	OnAudioCapture(InBuffer, inNumberFrames, 0.0, false); // need calculate timestamp
+
+	return noErr;
+}
+
+void Audio::FAudioCaptureIOS::AllocateBuffer(int32 SizeInBytes)
+{
+	const size_t CaptureBufferBytes = sizeof(AudioBufferList) + NumChannels * (sizeof(AudioBuffer) + SizeInBytes);
+	CaptureBuffer.SetNum(CaptureBufferBytes);
+
+	AudioBufferList* list = (AudioBufferList*)CaptureBuffer.GetData();
+	uint8* CaptureBufferData = CaptureBuffer.GetData() + sizeof(AudioBufferList) + sizeof(AudioBuffer);
+
+	list->mNumberBuffers = NumChannels;
+	for (int32 Index = 0; Index < NumChannels; ++Index)
+	{
+		list->mBuffers[Index].mNumberChannels = 1;
+		list->mBuffers[Index].mDataByteSize = (UInt32)SizeInBytes;
+		list->mBuffers[Index].mData = CaptureBufferData;
+
+		CaptureBufferData += (SizeInBytes + sizeof(AudioBuffer));
+	}
+}
+#endif

Source/RuntimeAudioImporter/Private/Sound/ImportedSoundWave.cpp
@@ -573,7 +573,8 @@ float UImportedSoundWave::GetDurationConst_Internal() const
 }
 
 float UImportedSoundWave::GetDuration()
-#if UE_VERSION_NEWER_THAN(5, 0, 0)
+#if UE_VERSION_OLDER_THAN(5, 0, 0)
+#else
 const
 #endif
 {

Source/RuntimeAudioImporter/Public/Sound/CapturableSoundWave.h
@@ -5,6 +5,9 @@
 #include "CoreMinimal.h"
 #if WITH_RUNTIMEAUDIOIMPORTER_CAPTURE_SUPPORT
 #include "AudioCaptureCore.h"
+#if PLATFORM_IOS
+#include "IOS/AudioCaptureIOS.h"
+#endif
 #endif
 #include "StreamingSoundWave.h"
 #include "CapturableSoundWave.generated.h"
@@ -86,5 +89,9 @@ private:
 #if WITH_RUNTIMEAUDIOIMPORTER_CAPTURE_SUPPORT
 	/** Audio capture instance */
 	Audio::FAudioCapture AudioCapture;
+#if PLATFORM_IOS && !PLATFORM_TVOS
+	/** Audio capture instance specific to iOS. Implemented manually due to the engine not properly supporting iOS audio capture at the moment */
+	Audio::FAudioCaptureIOS AudioCaptureIOS;
+#endif
 #endif
 };

Source/RuntimeAudioImporter/Public/Sound/IOS/AudioCaptureIOS.h
@@ -1,0 +1,56 @@
+// Georgy Treshchev 2023.
+
+#pragma once
+
+#if PLATFORM_IOS && !PLATFORM_TVOS
+#include "AudioCaptureCore.h"
+#include "Misc/EngineVersionComparison.h"
+#include "Math/UnrealMathUtility.h"
+
+#import <AudioToolbox/AudioToolbox.h>
+#import <AudioUnit/AudioUnit.h>
+#import <AVFoundation/AVFoundation.h>
+
+/**
+ * This recreates the FAudioCaptureAudioUnitStream iOS-specific audio capture implementation, but with fixes to make it work correctly on iOS and avoid crashes
+ */
+namespace Audio
+{
+	class FAudioCaptureIOS : public IAudioCaptureStream
+	{
+	public:
+		virtual bool GetCaptureDeviceInfo(Audio::FCaptureDeviceInfo& OutInfo, int32 DeviceIndex) override;
+		virtual bool OpenCaptureStream(const Audio::FAudioCaptureDeviceParams& InParams, Audio::FOnCaptureFunction InOnCapture, uint32 NumFramesDesired) override;
+		virtual bool CloseStream() override;
+		virtual bool StartStream() override;
+		virtual bool StopStream() override;
+		virtual bool AbortStream() override;
+		virtual bool GetStreamTime(double& OutStreamTime) override;
+		virtual int32 GetSampleRate() const override { return SampleRate; }
+		virtual bool IsStreamOpen() const override;
+		virtual bool IsCapturing() const override;
+		virtual void OnAudioCapture(void* InBuffer, uint32 InBufferFrames, double StreamTime, bool bOverflow) override;
+		virtual bool GetInputDevicesAvailable(TArray<FCaptureDeviceInfo>& OutDevices) override;
+#if UE_VERSION_NEWER_THAN(4, 25, 0)
+		virtual void SetHardwareFeatureEnabled(Audio::EHardwareInputFeature FeatureType, bool bEnabled) override;
+#endif
+
+		OSStatus OnCaptureCallback(AudioUnitRenderActionFlags* ioActionFlags, const AudioTimeStamp* inTimeStamp, UInt32 inBusNumber, UInt32 inNumberFrames, AudioBufferList* ioData);
+
+	private:
+		void AllocateBuffer(int32 SizeInBytes);
+
+		bool bIsStreamOpen = false;
+		bool bHasCaptureStarted = false;
+		AudioComponentInstance IOUnit;
+
+		int32 NumChannels = 1;
+		int32 SampleRate = 48000;
+
+		TArray<uint8> CaptureBuffer;
+		int32 BufferSize = 0;
+
+		FOnCaptureFunction OnCapture;
+	};
+}
+#endif

Source/RuntimeAudioImporter/Public/Sound/ImportedSoundWave.h
@@ -253,7 +253,6 @@ public:
 	 * Get the length of the sound wave, in seconds
 	 * @note This adds a duration offset (relevant if ReleasePlayedAudioData was used)
 	 */
-	UFUNCTION(BlueprintCallable, Category = "Imported Sound Wave|Info")
 	virtual float GetDuration()
 #if UE_VERSION_OLDER_THAN(5, 0, 0)
 	override;

Source/RuntimeAudioImporter/RuntimeAudioImporter.Build.cs
@@ -92,10 +92,19 @@ public class RuntimeAudioImporter : ModuleRules
 			else if (Target.Platform == UnrealTargetPlatform.IOS)
 			{
 				PrivateDependencyModuleNames.Add("AudioCaptureAudioUnit");
+				PrivateDependencyModuleNames.Add("Core");
+				PrivateDependencyModuleNames.Add("AudioCaptureCore");
+				PublicFrameworks.AddRange(new string[] { "CoreAudio", "AVFoundation", "AudioToolbox" });
 			}
 			else if (Target.Platform == UnrealTargetPlatform.Android)
 			{
-				PrivateDependencyModuleNames.Add("AudioCaptureAndroid");
+				PrivateDependencyModuleNames.AddRange(
+					new string[]
+					{
+						"AudioCaptureAndroid",
+						//"AndroidPermission"
+					}
+				);
 			}
 
 			PublicDependencyModuleNames.AddRange(

commit 08d1642cc43630508d4b2703f3fbea7c65654407

Properly unmute capturable sound wave (issue was introduced in bc628ba)

Source/RuntimeAudioImporter/Private/Sound/CapturableSoundWave.cpp
@@ -261,9 +261,9 @@ bool UCapturableSoundWave::ToggleMute(bool bMute)
 	else
 	{
 #if PLATFORM_IOS && !PLATFORM_TVOS
-		if (!AudioCaptureIOS.IsCapturing())
+		if (AudioCaptureIOS.IsCapturing())
 #else
-		if (!AudioCapture.IsCapturing())
+		if (AudioCapture.IsCapturing())
 #endif
 		{
 			UE_LOG(LogRuntimeAudioImporter, Error, TEXT("Unable to unmute as the stream for %s is already open"), *GetName());