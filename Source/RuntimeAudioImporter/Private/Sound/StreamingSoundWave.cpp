a15fae2657d5f0f3faac1306b388485aaa485ee3
Georgy Treshchev <gtreshchev@gmail.com>
2023-12-03 11:45:14 +0000

    Replace manual constructor with MakeWeakObjectPtr to deduce the initializer type

--- Source/RuntimeAudioImporter/Private/Sound/StreamingSoundWave.cpp
+++ Source/RuntimeAudioImporter/Private/Sound/StreamingSoundWave.cpp
@@ -224,7 +224,7 @@ void UStreamingSoundWave::PreAllocateAudioData(int64 NumOfBytesToPreAllocate, co
 {
 	if (IsInGameThread())
 	{
-		AsyncTask(ENamedThreads::AnyBackgroundHiPriTask, [WeakThis = TWeakObjectPtr<UStreamingSoundWave>(this), NumOfBytesToPreAllocate, Result]()
+		AsyncTask(ENamedThreads::AnyBackgroundHiPriTask, [WeakThis = MakeWeakObjectPtr(this), NumOfBytesToPreAllocate, Result]()
 		{
 			if (WeakThis.IsValid())
 			{
@@ -276,7 +276,7 @@ void UStreamingSoundWave::AppendAudioDataFromEncoded(TArray<uint8> AudioData, ER
 {
 	if (IsInGameThread())
 	{
-		AsyncTask(ENamedThreads::AnyBackgroundHiPriTask, [WeakThis = TWeakObjectPtr<UStreamingSoundWave>(this), AudioData = MoveTemp(AudioData), AudioFormat]() mutable
+		AsyncTask(ENamedThreads::AnyBackgroundHiPriTask, [WeakThis = MakeWeakObjectPtr(this), AudioData = MoveTemp(AudioData), AudioFormat]() mutable
 		{
 			if (WeakThis.IsValid())
 			{
@@ -317,7 +317,7 @@ void UStreamingSoundWave::AppendAudioDataFromRAW(TArray<uint8> RAWData, ERuntime
 {
 	if (IsInGameThread())
 	{
-		AsyncTask(ENamedThreads::AnyBackgroundHiPriTask, [WeakThis = TWeakObjectPtr<UStreamingSoundWave>(this), RAWData = MoveTemp(RAWData), RAWFormat, InSampleRate, NumOfChannels]() mutable
+		AsyncTask(ENamedThreads::AnyBackgroundHiPriTask, [WeakThis = MakeWeakObjectPtr(this), RAWData = MoveTemp(RAWData), RAWFormat, InSampleRate, NumOfChannels]() mutable
 		{
 			if (WeakThis.IsValid())
 			{
