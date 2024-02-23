0dd61dbcf6ab1a1f2c533fe033b4598979d0b989
Georgy Treshchev <gtreshchev@gmail.com>
2024-02-23 17:22:39 +0000

    Auto-add NSMicrophoneUsageDescription for iOS mic access prompt
    Automatically adding the NSMicrophoneUsageDescription plist data entry on editor startup for the iOS microphone access prompt to show up when using the capturable sound waves

--- Source/RuntimeAudioImporterEditor/Public/PreImportedSoundFactory.h
+++ Source/RuntimeAudioImporterEditor/Public/PreImportedSoundFactory.h
@@ -12,11 +12,8 @@
 #include "Factories/Factory.h"
 #include "PreImportedSoundFactory.generated.h"
 
-/** Declaring custom logging */
-DECLARE_LOG_CATEGORY_EXTERN(LogPreImportedSoundFactory, Log, All);
-
 /**
- * Factory for pre-importing audio files. Supports all formats from EAudioFormat, but OGG Vorbis is recommended due to its smaller size and better quality
+ * Factory for pre-importing audio files. Supports all formats from ERuntimeAudioFormat, but OGG Vorbis is recommended due to its size and quality balance
  */
 UCLASS()
 class RUNTIMEAUDIOIMPORTEREDITOR_API UPreImportedSoundFactory : public UFactory, public FReimportHandler
@@ -24,7 +21,6 @@ class RUNTIMEAUDIOIMPORTEREDITOR_API UPreImportedSoundFactory : public UFactory,
 	GENERATED_BODY()
 
 public:
-	/** Default constructor */
 	UPreImportedSoundFactory();
 
 	//~ Begin UFactory Interface.
